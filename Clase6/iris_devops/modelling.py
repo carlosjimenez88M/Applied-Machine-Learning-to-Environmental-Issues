632fbc025a9b6d4a1aa81e504287ee9566a6426d
carlos_raspberrypi5 <danieljimenez88m@gmail.com>
2024-07-24 14:57:16 -0500

    Adding methods

---
+++ Clase6/iris_devops/modelling.py
@@ -0,0 +1,22 @@
+#=============================================================================#
+#         Design Machine learning software model to Classification problem.   #
+#.                   Universidad Nacional de Colombia.                        #
+#.                              2024.                                         #
+#=============================================================================#
+
+
+
+# Import Libraries ------------
+import pandas as pd 
+import numpy as numpy
+import matplotlib.pyplot as plt 
+from pathlib import Path
+import urllib.request
+
+
+# download dataset ---------
+
+def download_dataset():
+    ''' función para descargar bases de datos'''
+    
+
